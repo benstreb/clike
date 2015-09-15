4cc8db5988211a4b563b65a7a9e6ae4d3e6ce6d0
Benjamin Streb <Bstreb@gmail.com>
2015-09-15 08:41:09 -0700

    Added a full module
    That should be sufficient for code generation

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -3,8 +3,25 @@ module Codegen
     ) where
 
 import LLVM.General.AST
-import LLVM.General.AST.Instruction
 import LLVM.General.AST.Constant
+import LLVM.General.AST.Global as Global
+import LLVM.General.AST.Type as Type
+
+return0 :: Terminator
+return0 = Ret (Just (ConstantOperand (Int 0 0))) []
 
 block :: BasicBlock
-block = BasicBlock (Name "block") [] (Do (Ret (Just (ConstantOperand (Int 0 0))) []))
+block = BasicBlock (Name "block") [] (Do return0)
+
+func :: Definition
+func = GlobalDefinition $ functionDefaults
+    { Global.returnType = Type.i32
+    , Global.name = Name "func"
+    , Global.basicBlocks = [block]
+    }
+
+mod :: Module
+mod = defaultModule
+    { moduleName = "Test"
+    , moduleDefinitions = [func]
+    }
