2c48966d16e251d92c949671737ae6a220b095f1
Benjamin Streb <Bstreb@gmail.com>
2015-09-25 00:15:29 -0700

    Added first pass at top level assignments
    For the Parse Tree to intermediate representation transformation -
    semantic analysis, if I'm not mistaken

--- src/IR.hs
+++ src/IR.hs
@@ -1,9 +1,26 @@
 module IR where
 
-import Parser (ParseTree)
+import Control.Applicative
+import Control.Monad.Except
+import Parser ( ParseTree( Assign, Id, Num ) )
 
 data AST = Root
          deriving (Eq, Show)
 
+data Assign = Assign String Value
+            deriving (Eq, Show)
+
+data Value = Int Integer
+           deriving (Eq, Show)
+
 fromParseTree :: ParseTree -> Either String AST
 fromParseTree parseTree = Right Root
+
+topLevel :: [ParseTree] -> Either String [Assign]
+topLevel assigns = forM assigns matchAssign
+    where
+        matchAssign :: ParseTree -> Either String IR.Assign
+        matchAssign (Parser.Assign (Id name) valueTree) = case valueTree of
+            Parser.Num n -> return (IR.Assign name (Int n))
+            _ -> throwError "Not Implemented Yet: Unsupported LHS type"
+        matchAssign _ = throwError "ICE: Malformed assignment statement"
--- test/Spec.hs
+++ test/Spec.hs
@@ -30,6 +30,9 @@ irSpec = do
     describe "root" $ do
         it "doesn't fail, given an empty AST" $ do
             IR.fromParseTree (Root []) `shouldNotSatisfy` isError
+    describe "topLevel" $ do
+        it "handles assignment statements" $
+            IR.topLevel [Assign (Id "value") (Num 3)] `shouldBe` Right [IR.Assign "value" (IR.Int 3)]
 
 parserSpec :: Spec
 parserSpec = do
